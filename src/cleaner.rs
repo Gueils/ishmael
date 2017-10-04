ddcd378600c534463fbb02adc0d714e4c5fc6c20
Mr. Eyebrows <cejas@icalialabs.com>
2017-10-04 10:46:20 -0500

    PR feature
    * Reverts PR to forked repo.
    * Cleans repo after attempted dockerization

--- src/cleaner.rs
+++ src/cleaner.rs
@@ -1,15 +1,34 @@
+use std::process::Command;
+
 /// Ishmael::Cleaner
 /// Allows to clear /repos directory.
 ///
 /// Usage:
 /// ```
 /// ```
-pub struct Cleaner {}
+pub struct Cleaner<'a> {
+    name: &'a String,
+}
 
-impl Cleaner {
-    pub fn new() -> Cleaner {
-        Cleaner {}
+impl<'a> Cleaner<'a> {
+    pub fn new(name: &String) -> Cleaner {
+        Cleaner {
+            name: name
+        }
     }
 
-    pub fn process(&self) {}
+    pub fn process(&self) -> bool {
+        let dir = format!("/repos/{}", &self.name);
+
+        let output = Command::new("rm")
+            .current_dir("/")
+            .args(&[
+                 "-R",
+                 dir.as_str()
+            ])
+            .output()
+            .expect("God damn! We couldn't removed the repo directory");
+
+        output.status.success()
+    }
 }
